38b3b533b72ab8ac96476b92ad16a3e2913c607a
Yongjie Deng <130525414+yjdeng9@users.noreply.github.com>
2024-02-23 10:52:59 +0000

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,20 @@
+conda create -n tf python=3.6
+
+pip install pyteomics
+pip install lxml
+pip install tensorflow==2.2
+pip install keras==2.3
+pip install pandas
+pip install numpy
+pip install scikit-learn
+pip install scikit-image
+pip install Pillow
+pip install matplotlib
+pip install efficientnet
+pip install tqdm
+pip install umap-learn
+
+
+
+apt-get install libopenjp2-7
+yum install openjpeg2
